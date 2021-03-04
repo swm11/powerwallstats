3e3890bb8138e9f37658f5b8b9c97e892acf57d5
Simon Moore <simon.moore@cl.cam.ac.uk>
2021-03-04 20:50:03 +0000

    Initial

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# powerwallstats change log
+
+## 1.0.0 - 2021-03-04
+
+* Initial version
+
+
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+major_version = 1
+minor_version = 0
+build_version = 0
+
+version = str(major_version) + '.' + str(minor_version) + '.' + str(build_version)
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='powerwallstats',
+    version=version,
+    description='powerwallstats: python library to read statistics from a Powerwall 2 gateway',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Simon Moore',
+    url='https://github.com/swm11/powerwallstats',
+    license='BSD 2-Clause',
+    packages=('powerwallstats',),
+    install_requires=('requests',),
+)
