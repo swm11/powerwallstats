c32da51dbaef0e211e753d16d72dfca0e0814be7
Simon Moore <simon.moore@cl.cam.ac.uk>
2021-04-14 16:55:03 +0100

    Untabify

--- examples/show_powerwallstats.py
+++ examples/show_powerwallstats.py
@@ -8,24 +8,23 @@ customer_email = 'Your customer email address goes here (the one to login to you
 customer_password = 'Your customer password goes here (the one to login to your powerwall)'
 
 def main():
-	# instantiate a powerwallstats object and provide credentails:
-	pw = powerwallstats(powerwallip, customer_email, customer_password)
+    # instantiate a powerwallstats object and provide credentails:
+    pw = powerwallstats(powerwallip, customer_email, customer_password)
 
-	# power source names:
-	source_names = ['battery', # battery power, where positive is discharging
-					'site',    # grid power, where positive is import
-					'load',    # house power consumption, always positive
-					'solar']   # solar power, where positive is solar energy provided  
+    # power source names:
+    source_names = ['battery', # battery power, where positive is discharging
+                    'site',    # grid power, where positive is import
+                    'load',    # house power consumption, always positive
+                    'solar']   # solar power, where positive is solar energy provided  
 
-	# read some values
-	while True:
-		print("battery level: %d%%" % (pw.battery_level()))
-		meters = pw.meters()
-		for src in source_names:
-			print("%s %5.3f kW" % (src, float(meters[src]['instant_power'])/1000.0))
-		print()
-		time.sleep(1)
+    # read some values
+    while True:
+        print("battery level: %d%%" % (pw.battery_level()))
+        meters = pw.meters()
+        for src in source_names:
+            print("%s %5.3f kW" % (src, float(meters[src]['instant_power'])/1000.0))
+        print()
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
-
